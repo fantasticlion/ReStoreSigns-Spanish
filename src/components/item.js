c7ef607a04dc9a70c96f570aadc692733f072611
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-09-12 15:57:47 -0500

    Add files via upload

---
+++ src/components/item.js
@@ -0,0 +1,13 @@
+export default function ProductItem(props) {   
+  return (
+    <div className="item">
+      <img src={props.info["image_link"]} alt={props.info["name"]}></img>
+      <button onClick={() => props.setStateOfParent(props.info["name"], props.info["unit_price"], 'add')}>
+        Add
+      </button>
+      <button onClick={() => props.setStateOfParent(props.info["name"], props.info["unit_price"], 'remove')}>
+        Remove
+      </button>
+    </div>
+  );
+}
